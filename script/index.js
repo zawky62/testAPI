60460d3076e9c3149d56cd1e0df0e7ba7257d9fe
Bernard Florian <flo.b6259@gmail.com>
2021-11-15 15:55:03 +0100

    Avancement code

--- script/index.js
+++ script/index.js
@@ -7,16 +7,45 @@ function Main(){
 
     const URL = `https://deces.matchid.io/deces/api/v1/agg?firstName=${firstName}&lastName=${lastName}&deathCity=${city}&deathCountry=${country}&fuzzy=false&aggs=firstName`;
     console.log(URL);
+    
 
-    document.location(URL)
+    ouvrirUrl(URL);
+    //getData(URL);
+    //var d1 = document.createElement("")
 };
 
-function getData() {
-
-
+function getData(jsonObj, URL) {
+    request.open('GET', URL);
+    var myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['response.total'];
+    header.appendChild(myH1);
+ 
 };
 
-function buildURL(lastName, firstName, country, city){
-    //https://deces.matchid.io/deces/api/v1/agg?firstName=Raphael&deathCountry=France&fuzzy=false&aggs=firstName
+function ouvrirUrl(URL) {
+
+    window.open(URL);
 };
+
+
+
+// function getData(URL){
+
+//     fetch(URL)
+//     .then(function(res) {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//     })
+//     .then(function(value) {
+//       console.log(value);
+//     })
+//     .catch(function(err) {
+//       // Une erreur est survenue
+//     });
+
+
+
+
+
 
